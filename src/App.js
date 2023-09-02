cb31f60bb418b6fce4d88e49c58d6c2c7be06055
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-09-02 20:52:11 +0100

    Created Components folder and added components to my app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Post } from "./components/Post";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div>
+      <Signin/>
+      <Signup/>
+      <Post/>
       
     </div>
   );
