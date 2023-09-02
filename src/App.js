8f1a1b2bfa4999a7c27a916bfec51f8979f6fe50
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-09-02 21:24:49 +0100

    Commit:Installed react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from "react";
 import HomePage from "./components/HomePage";
-import { Post } from "./components/Post";
+import Post  from "./components/Post";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div>
-      <HomePage/>
-      <Signin/>
-      <Signup/>
-      <Post/>
+      <Switch>
+        <Route path="/">
+         <HomePage/>
+       </Route>
+       <Route path="/Signin">
+         <Signin/>
+       </Route>
+       <Route path="/Signup">
+         <Signup/>
+       </Route>
+       <Route path="/Post">
+         <Post/>
+       </Route>
+
+      </Switch>
+      
       
     </div>
+    </Router>
   );
 }
 
