889e2fb6150aba4da1a31522cd1b4ce9c355d5a0
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-09-03 15:36:30 +0100

    Nav completed.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <h1 className='font-customFont font-extrabold text-6xl text-center bg-black '>broo</h1> 
+        <h1 className='bg-black h-[3.7rem] text-white font-customFont text-center pt-3 text-2xl'>PicPlus+</h1> 
    </div>
   )
 }
