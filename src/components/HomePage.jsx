ce9d9bb147cb5077fece497b61b22193444df97e
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-09-03 16:49:58 +0100

    Commit :EDited the navbar

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,13 +4,13 @@ import Pic from '../assets/picturee2.png';
 const HomePage = () => {
   return (
     <div class="md:flex">
-  
-      <div class="md:w-1/4">
-        dakmmlsdm'l
+      <div class="md:w-1/4 pt-[3rem] px-9">
+        <h1 className='text-3xl text-blue-800 font-bold pb-3 border-b border-blue-950'>Post your Pictures</h1>
+        <p className='text-blue-500 font-semibold '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias culpa rem eligendi est aut a. Vel, hic qui dolor cumque nulla assumenda voluptas aliquam cum asperiores omnis quod libero cupiditate.</p>
       </div>
 
-      <div class="w-3/4 pt-[3rem]"> {/* Change the width to 3/4 */}
-        <img src={Pic} alt='pix' className='w-full lg:ml-[0rem] ' /> {/* Change width to w-full */}
+      <div class="w-full md:w-3/4 pt-[3rem]"> {/* Use w-full for xs and w-3/4 for md and larger screens */}
+        <img src={Pic} alt='pix' className='w-full' /> {/* Use w-full to take up the full width */}
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <h1 className="bg-gradient-to-r from-blue-100 via-blue-400 to-blue-800 h-[4rem] pt-3 text-3xl font-customFont text-gray-200 text-center">PicPlus+</h1>
-  
+        <h1 className="bg-gradient-to-r from-indigo-200 via-indigo-400 to-indigo-600 h-[4rem] pt-3 text-3xl font-customFont text-gray-200 text-center">PicPlus+</h1>
    </div>
   )
 }
