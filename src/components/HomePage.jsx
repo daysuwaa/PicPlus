4b7a680b8671164799db39a5116e4e522fa8532c
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-09-03 16:30:29 +0100

    Picture added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Pic from '../assets/camera.png'
+import React from 'react';
+import Pic from '../assets/picturee2.png';
 
 const HomePage = () => {
   return (
-    <div>
-    <img src={Pic} alt='pixx' />
+    <div class="md:flex">
+  
+      <div class="md:w-1/4">
+        dakmmlsdm'l
+      </div>
+
+      <div class="w-3/4 pt-[3rem]"> {/* Change the width to 3/4 */}
+        <img src={Pic} alt='pix' className='w-full lg:ml-[0rem] ' /> {/* Change width to w-full */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
