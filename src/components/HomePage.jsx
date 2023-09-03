5de188b5649968d0bfc8382d70a3847cdeaa248a
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-09-03 18:18:40 +0100

    Commit : Home page done.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,9 +11,15 @@ const HomePage = () => {
         </h1>
         <p className="text-indigo-800 font-semibold mt-8">
           Discover the beauty of moments captured in time. Share your memories with the world.<br></br>
+          <div className="relative">
+          <p className="text-indigo-800 font-semibold mt-5 pb-3 relative cursor-pointer group w-1/3 lg:w-1/2 ">
           Let's get started
+       <span className="absolute inset-x-0 h-1 bg-indigo-700 bottom-0 transition-transform transform scale-x-0 origin-left group-hover:scale-x-100"></span>
+       </p>
+    </div>
+
         </p>
-        <div className="flex mt-12">
+        <div className="flex mt-9">
           <Link
             to="/PicPlus/signin"
             className="bg-indigo-700 hover:bg-indigo-800 text-white font-semibold text-sm py-3 px-6 rounded-3xl mr-4 transition duration-300 ease-in-out"
