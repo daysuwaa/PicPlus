c0011ca24414b8a954f2e78f9fdba66d8ac2a528
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-09-03 15:47:06 +0100

    Craeted assets and added a picture

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <div className='bg-black h-[4rem] text-white font-customFont text-center pt-4 text-2xl'> PicPlus+
-        {/* <h1 className='font-customFont font-extrabold text-6xl text-center '></h1> */}
 
-
-      </div>
     </div>
   )
 }
