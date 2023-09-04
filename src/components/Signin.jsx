eb2aef5f16eaa9db5179dc3c68006d20b6bd6021
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-09-04 19:14:32 +0100

    Finished with sign in

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -13,78 +13,79 @@ const SignIn = () => {
   };
 
   return (
-    
+    <div>
+    <h2 className='text-2xl text-center mb-4 pt-5 font-semibold'>Sign In</h2>
     <div className="md:flex">
-    <h2 className="text-2xl font-bold text-center mb-4">Sign In</h2>
-      <div className="w-full md:w-1/2 pt-[5rem]">
-        <img src={Picture2} alt="pix" className="w-full rounded " />
+      {/* Image section */}
+     
+      <div className="w-full md:w-1/2">
+        <img src={Picture2} alt="pix" className="w-full rounded md:py-[5rem] lg:py-[0rem]" />
       </div>
 
-      <div className=" w-full max-w-sm mx-auto  h-2">
-           <form className="lg:bg-gray-100 lg:shadow-md rounded px-8 pt-7 pb-8 mb-4">
-        
-           <div className="mb-4 text-black">
-  <div className="relative">
-    <label className='pb-2'>Email:
-    <span className="absolute inset-y-0 left-0 flex items-center pl-3 pt-7">
-      <FiMail className="text-black font-bold" /> {/* Email icon */}
-    </span>
-    <input
-      className="shadow appearance-none border border-black rounded w-full py-2 pl-10 pr-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
-      type="text"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-    />
-    </label>
-  </div>
-</div>
+      {/* Form section */}
+      <div className="w-full lg:w-[38rem] md:w-1/2 px-[2rem] md:py-[9rem] lg:py-[11rem] h-2 ">
+        <form className="  rounded px-8 pt-7 pb-8 mb-4 bg-gray-100">
+          {/* Email section */}
+          <div className="mb-4 text-black">
+            <div className="relative">
+              <label className='pb-2'>Email:
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3 pt-7">
+                  <FiMail className="text-black font-bold" />
+                </span>
+                <input
+                  className="shadow appearance-none border border-black rounded w-full py-2 pl-10 pr-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+            </div>
+          </div>
 
-<div className="relative">
-<label className='pb-2 '>Password:
-  <span className="absolute inset-y-0 left-0 flex items-center pl-3 pt-7 ">
-    <FiLock className="text-black font-extrabold" /> {/* Password icon */}
-  </span>
-  <input
-    className="shadow appearance-none border border-black rounded w-full py-2 pl-10 pr-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
-    type={showPassword ? 'text' : 'password'}
-   
-    value={password}
-    onChange={(e) => setPassword(e.target.value)}
-  />
-  </label>
-  <span
-    className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer pt-7"
-    onClick={handlePasswordToggle}
-  >
-    {showPassword ? <FiEyeOff /> : <FiEye />}
-  </span>
-</div>
+          {/* Password section */}
+          <div className="relative">
+            <label className='pb-2'>Password:
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3 pt-7">
+                <FiLock className="text-black font-extrabold" />
+              </span>
+              <input
+                className="shadow appearance-none border border-black rounded w-full py-2 pl-10 pr-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span
+                className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer pt-7"
+                onClick={handlePasswordToggle}
+              >
+                {showPassword ? <FiEyeOff /> : <FiEye />}
+              </span>
+            </label>
+          </div>
 
-          
-<div className="flex items-center justify-center mt-6">
-  <button
-    className="bg-indigo-600 hover:bg-blue-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-[400px] rounded h-11"
-    type="button"
-  >
-    <p className='text-sm'>Sign In</p>
-  </button>
-</div>
-<p className='pt-4'>Don't have an account?
-<Link 
-            to="/PicPlus/signup"
-            className=" text-blue-700 border-b underline pl-2"
-          >
-           Sign Up
-          </Link>
-</p>
-      </form>
+          {/* Sign-in button */}
+          <div className="flex items-center justify-center mt-6">
+            <button
+              className="bg-indigo-600 hover:bg-blue-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-[400px] rounded h-11"
+              type="button"
+            >
+              <p className='text-sm'>Sign In</p>
+            </button>
+          </div>
+
+          {/* Text */}
+          <p className='pt-4 text-center'>Don't have an account?
+            <Link
+              to="/PicPlus/signup"
+              className="text-blue-700 border-b underline pl-2"
+            >
+              Sign Up
+            </Link>
+          </p>
+        </form>
+      </div>
+      </div>
     </div>
-
-   
-
-    
-    </div>
-    
   );
 };
 
